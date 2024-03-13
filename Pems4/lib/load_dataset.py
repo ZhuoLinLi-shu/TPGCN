f57a1daed9a275b376d31e37b0b0c0cabc5316f2
zhuolin li <41195895+ZhuoLinLi-shu@users.noreply.github.com>
2024-03-13 19:02:45 +0800

    Add files via upload

---
+++ Pems4/lib/load_dataset.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+
+def load_st_dataset(dataset):
+    #output B, N, D
+    if dataset == 'PEMSD4':
+        # todo 这里很神奇的就是，按道理来讲应该加上..,但是加上就错了，去掉就对，我也是无语了
+        data_path = os.path.join('data/PeMSD4/pems04.npz')
+        data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
+    elif dataset == 'PEMSD8':
+        data_path = os.path.join('data/PeMSD8/pems08.npz')
+        data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
+    elif dataset == 'PEMSD3':
+        data_path = os.path.join('data/PeMSD3/PEMS03.npz')
+        data = np.load(data_path)['data'][:, :, 0]
+    elif dataset == 'PEMSD7':
+        data_path = os.path.join('data/PeMSD7/PEMS07.npz')
+        data = np.load(data_path)['data'][:, :, 0]
+    else:
+        raise ValueError
+    if len(data.shape) == 2:
+        data = np.expand_dims(data, axis=-1)
+    print('Load %s Dataset shaped: ' % dataset, data.shape, data.max(), data.min(), data.mean(), np.median(data))
+    return data
